936532e5a1e65a9d171a0d165c5d5c9f6e8e160e
kaicherry <kaicherry@example.com>
2026-07-12 19:19:17 -0700

    Updated/replaced firmware

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2026-07-12T09:57:45.608039"
+#define BUILD_TIME "2026-07-12T19:13:38.028047"
 #define BUILD_VERSION "0.1.8c-PSTH"
-#define BUILD_COMMIT "12303a0"
+#define BUILD_COMMIT "8e29d39"
